a4f4e90f153f086d420533f7bb71b12d6b212f12
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-06 14:39:15 +0100

    start mileston 4

--- js/script.js
+++ js/script.js
@@ -131,9 +131,9 @@ const app = new Vue(
   }
 )
 
-// Milestone 3
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
 
 
 // associando counter ed index, se questi corrispondono allora aggiungo la mia classe clicked per mantenerli colorati di grigio 
