aea0e40d554fbe0b1b9a665027aa099a67032ce8
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-07 11:59:08 +0100

    add dropdown menu

--- js/script.js
+++ js/script.js
@@ -16,16 +16,19 @@ const app = new Vue(
               date: "10/01/2020 15:30:55",
               text: "Hai portato a spasso il cane?",
               status: "sent",
+              dropMenu: false
             },
             {
               date: "10/01/2020 15:50:00",
               text: "Ricordati di dargli da mangiare",
               status: "sent",
+              dropMenu: false
             },
             {
               date: "10/01/2020 16:15:22",
               text: "Tutto fatto!",
               status: "received",
+              dropMenu: false
             },
           ],
         },
@@ -38,16 +41,19 @@ const app = new Vue(
               date: "20/03/2020 16:30:00",
               text: "Ciao come stai?",
               status: "sent",
+              dropMenu: false
             },
             {
               date: "20/03/2020 16:30:55",
               text: "Bene grazie! Stasera ci vediamo?",
               status: "received",
+              dropMenu: false
             },
             {
               date: "20/03/2020 16:35:00",
               text: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "sent",
+              dropMenu: false
             },
           ],
         },
@@ -61,16 +67,19 @@ const app = new Vue(
               date: "28/03/2020 10:10:40",
               text: "La Marianna va in campagna",
               status: "received",
+              dropMenu: false
             },
             {
               date: "28/03/2020 10:20:10",
               text: "Sicuro di non aver sbagliato chat?",
               status: "sent",
+              dropMenu: false
             },
             {
               date: "28/03/2020 16:15:22",
               text: "Ah scusa!",
               status: "received",
+              dropMenu: false
             },
           ],
         },
@@ -83,11 +92,13 @@ const app = new Vue(
               date: "10/01/2020 15:30:55",
               text: "Lo sai che ha aperto una nuova pizzeria?",
               status: "sent",
+              dropMenu: false
             },
             {
               date: "10/01/2020 15:50:00",
               text: "Si, ma preferirei andare al cinema",
               status: "received",
+              dropMenu: false
             },
           ],
         },
@@ -106,6 +117,7 @@ const app = new Vue(
             date: "10/01/2020 15:30:55",
             text: this.newMex,
             status: "received",
+            dropMenu: false
           }
           this.contacts[index].messages.push(newMex);
           this.newMex = ''; 
@@ -118,17 +130,29 @@ const app = new Vue(
             date: "10/01/2020 15:30:55",
             text: 'ok.',
             status: "sent",
+            dropMenu: false
           }
           this.contacts[index].messages.push(newMex);
           this.newMex = ''; 
         }, 1000);
       },
       searching: function () {
+        // giro su tutti i contacts
+        this.contacts.forEach(element => {
+          // se il v-model corrisponde al nome allora visible è true
+          if (element.name.toLowerCase().includes(this.nameFilter.toLowerCase())) {
+            element.visible = true;
+          } else {
+            element.visible = false;
+          }
+        });
       },
       dropdownMenu: function () {
-        let menu = document.getElementById('dropdown_content');
-        menu.classList.add('hide');
-        console.log(menu);
+        // this.dropMenu = !this.dropMenu;
+        // questa funzione la metto direttamente nel click dell'icona per facilitare tutto
+      },
+      deleteMessage: function () {
+        
       }
     },
     created() {
@@ -153,8 +177,7 @@ const app = new Vue(
   }
 )
 
-// Milestone 4
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina) ----->>>> milestone non completata
+
 
 
 // Milestone 5
